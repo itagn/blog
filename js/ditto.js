cb87f344061ba0499545a76ac4a08672d615fa52
itagn <itagn@foxmail.com>
2018-07-30 16:46:47 +0800

    修改找不到图片的bug

--- js/ditto.js
+++ js/ditto.js
@@ -289,7 +289,7 @@ function router() {
       window.disqus_shortname = 'es6';
       window.disqus_identifier = (location.hash ? location.hash.replace("#", "") : 'READEME');
       window.disqus_title = $(ditto.content_id + " h1").text();
-      window.disqus_url = 'http://itagn.xyz/' + (location.hash ? location.hash.replace("#", "") : 'README');
+      window.disqus_url = 'https://itagn.github.io/blog/' + (location.hash ? location.hash.replace("#", "") : 'README');
 
       // http://docs.disqus.com/developers/universal/
       (function() {
